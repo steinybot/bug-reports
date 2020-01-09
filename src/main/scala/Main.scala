cc6e2518beed22cc601b7af03b1f0929f53f118d
Jason Pickens <jasonpickensnz@gmail.com>
2020-01-10 09:49:22 +1300

    Add reproduction for oracle jdk bug 9063277

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,21 @@
+import java.time._
+import java.time.zone.ZoneOffsetTransition
+import java.time.zone.ZoneOffsetTransitionRule
+import java.time.zone.ZoneRules
+import java.util.Collections
+
+// Bug report for bug 9063277
 object Main extends App {
 
+  val maxLocalDateTime: Instant = LocalDateTime.of(Year.MAX_VALUE, 12, 31, 23, 59, 59, 999999999).toInstant(ZoneOffset.UTC)
+  val offsetZero: ZoneOffset = ZoneOffset.ofHours(0)
+  val offsetPlusOneHour: ZoneOffset = ZoneOffset.ofHours(1)
+  val zoneRulesA: ZoneRules = ZoneRules.of(offsetPlusOneHour)
+  val transition: ZoneOffsetTransition = ZoneOffsetTransition.of(LocalDateTime.ofEpochSecond(0, 0, offsetZero), offsetZero, offsetPlusOneHour)
+  val transitionRule: ZoneOffsetTransitionRule = ZoneOffsetTransitionRule.of(Month.JANUARY, 1, DayOfWeek.SUNDAY, LocalTime.MIDNIGHT, true, ZoneOffsetTransitionRule.TimeDefinition.STANDARD, offsetZero, offsetZero, offsetPlusOneHour)
+  val zoneRulesB: ZoneRules = ZoneRules.of(offsetZero, offsetZero, Collections.singletonList(transition), Collections.singletonList(transition), Collections.singletonList(transitionRule))
+  val offsetA: ZoneOffset = zoneRulesA.getOffset(maxLocalDateTime)
+  val offsetB: ZoneOffset = zoneRulesB.getOffset(maxLocalDateTime)
+  val areEqual: Boolean = offsetA == offsetB
+  System.out.println(areEqual)
 }
