c39193645805144377d5de5af721a91f6cde87c9
Jason Pickens <jasonpickensnz@gmail.com>
2020-01-20 11:16:09 +1300

    Reproduce bug

--- src/main/scala/Show.scala
+++ src/main/scala/Show.scala
@@ -1,11 +1,10 @@
-import scala.annotation.implicitNotFound
+package com.lightbend.console.refined
 
-@implicitNotFound(
-  "Could not find an implicit Show for type ${T}, predicate ${P} and result ${R}. " +
-    "You may want to define it as an implicit function that is polymorphic function over R."
+import scala.annotation.{implicitAmbiguous, implicitNotFound}
+
+@implicitNotFound("""Could not find an implicit Show for type ${T}, predicate ${P} and result ${R}.
+                    | You may want to define it as an implicit function that is polymorphic function over R.""".stripMargin)
+@implicitAmbiguous(
+  "Found ambiguous implicit Show for type ${T}, predicate ${P} and result ${R}. You should only have for each combination of T and P."
 )
-trait Show[T, P, R] {
-
-  val longString = "Could not find an implicit Show for type ${T}, predicate ${P} and result ${R}. " +
-    "You may want to define it as an implicit function that is polymorphic function over R."
-}
+trait Show[T, P, R]
