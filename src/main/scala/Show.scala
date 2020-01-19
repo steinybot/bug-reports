61820cec1ae2455cde21907035173d0709ace870
Jason Pickens <jasonpickensnz@gmail.com>
2020-01-20 11:17:05 +1300

    Simplify

--- src/main/scala/Show.scala
+++ src/main/scala/Show.scala
@@ -1,10 +1,5 @@
-package com.lightbend.console.refined
-
-import scala.annotation.{implicitAmbiguous, implicitNotFound}
+import scala.annotation.implicitNotFound
 
 @implicitNotFound("""Could not find an implicit Show for type ${T}, predicate ${P} and result ${R}.
                     | You may want to define it as an implicit function that is polymorphic function over R.""".stripMargin)
-@implicitAmbiguous(
-  "Found ambiguous implicit Show for type ${T}, predicate ${P} and result ${R}. You should only have for each combination of T and P."
-)
 trait Show[T, P, R]
