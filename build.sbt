c2c3f91b05b4464e3e35bb2efa7a5eec3e859e00
Jason Pickens <jasonpickensnz@gmail.com>
2020-01-20 09:09:07 +1300

    Add example of overflow

--- build.sbt
+++ build.sbt
@@ -1,1 +1,11 @@
 scalaVersion := "2.13.1"
+
+val hedgehogVersion = "452df95337eb6d29485f34cd30e23e584b3987f0"
+
+libraryDependencies ++= Seq(
+  "qa.hedgehog" %% "hedgehog-core" % hedgehogVersion,
+  "qa.hedgehog" %% "hedgehog-runner" % hedgehogVersion,
+  "qa.hedgehog" %% "hedgehog-sbt" % hedgehogVersion
+)
+
+resolvers += "bintray-scala-hedgehog" at "https://dl.bintray.com/hedgehogqa/scala-hedgehog"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,28 @@
-object Main extends App {
+import hedgehog.runner._
+import hedgehog._
+import hedgehog.core.Result.Success
+import hedgehog.core.{CoverPercentage, LabelName, Seed, SuccessCount}
 
+object Main extends Properties {
+
+  override def tests: List[Test] = List(
+    property("frequency handles large weights", oops)
+      .config(c => c.copy(testLimit = SuccessCount(100000))),
+  )
+
+  private def oops: Property = {
+    for {
+      size <- Gen.int(Range.constant(0, Size.max)).forAll
+      seed <- Gen.long(Range.constant(Long.MinValue, Long.MaxValue)).forAll
+    } yield {
+      val result = Gen.frequency1(
+        (Int.MaxValue, Gen.constant(true)),
+        (Int.MaxValue, Gen.constant(false))
+      ).run(Size(size), Seed.fromLong(seed))
+      result.value._2 ==== Some(true)
+    }
+  }.cover(CoverPercentage(0.25), LabelName("This should fail some of the time."), {
+    case Success => false
+    case _ => true
+  })
 }
