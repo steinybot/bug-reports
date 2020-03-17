ee186c03bae6fa51de6a24ee80338873d92df3c5
Jason Pickens <jasonpickensnz@gmail.com>
2020-03-17 18:23:43 +1300

    foo

--- client/src/test.ts
+++ client/src/test.ts
@@ -1,6 +1,6 @@
 import * as grpcWeb from 'grpc-web';
-import {MetricsProtoServiceClient} from 'com/lightbend/console/api/MetricsServiceClientPb';
-import {GetMetricsRequest, MetricsResponse} from 'com/lightbend/console/api/metrics_pb';
+import {MetricsProtoServiceClient} from 'api/MetricsServiceClientPb';
+import {GetMetricsRequest, MetricsResponse} from 'api/metrics_pb';
 
 const metricsService = new MetricsProtoServiceClient('http://localhost:8080', null, null);
 
---
+++ server/src/MetricsServiceClientPb.ts
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for com.lightbend.console.api
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+
+import {
+  GetMetricsRequest,
+  MetricsResponse} from './metrics_pb';
+
+export class MetricsProtoServiceClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'binary';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoGetMetrics = new grpcWeb.AbstractClientBase.MethodInfo(
+    MetricsResponse,
+    (request: GetMetricsRequest) => {
+      return request.serializeBinary();
+    },
+    MetricsResponse.deserializeBinary
+  );
+
+  getMetrics(
+    request: GetMetricsRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: MetricsResponse) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/com.lightbend.console.api.MetricsProtoService/GetMetrics',
+      request,
+      metadata || {},
+      this.methodInfoGetMetrics,
+      callback);
+  }
+
+}
+
