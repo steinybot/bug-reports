41377cbabfa956f3bfb0bb0757003b0bf7933301
Jason Pickens <jasonpickensnz@gmail.com>
2021-02-10 09:20:32 +1300

    Add Scala.js tutorial

---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,15 @@
+package tutorial.webapp
+
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+object TutorialApp {
+  def main(args: Array[String]): Unit = {
+    appendPar(document.body, "Hello World")
+  }
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
+  }
+}
