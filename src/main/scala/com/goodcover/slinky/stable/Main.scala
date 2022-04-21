c33f16357d31506e402fce7213ea3ea57dac9254
Jason Pickens <jasonpickensnz@gmail.com>
2022-04-21 13:41:32 +1200

    Minimize

--- src/main/scala/com/goodcover/slinky/stable/Main.scala
+++ src/main/scala/com/goodcover/slinky/stable/Main.scala
@@ -1,11 +1,13 @@
 package com.goodcover.slinky.stable
 
+// Making B invariant fixed it.
 sealed trait ImplicitlyStable[A, +B]
 
 object ImplicitlyStable {
 
   implicit def convertibleToStable[A, B](implicit
                                          aToB: A => B,
+                                         // Removing bToB fixes it.
                                          bToB: ImplicitlyStable[B, B]
                                         ): ImplicitlyStable[A, B] = ???
 }
