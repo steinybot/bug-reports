7d25e5692aa47297abc8a6278f0a5fca56cf35ef
Jason Pickens <jasonpickensnz@gmail.com>
2022-07-25 20:11:48 +1200

    Reproduce abstract type pattern error

--- build.sbt
+++ build.sbt
@@ -1,1 +1,8 @@
 scalaVersion := "2.13.8"
+
+libraryDependencies += "io.estatico" %% "newtype" % "0.4.4"
+
+scalacOptions ++= Seq(
+  "-Ymacro-annotations",
+  "-Werror"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+import io.estatico.newtype.macros.newsubtype
+
 object Main extends App {
 
+  @newsubtype final case class Stable[A] private (unwrap: A)
+
+  def foo(a: Any) = a match {
+    case s: Stable[_] => true
+    case _ => false
+  }
 }
