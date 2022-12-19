38c70e71616152720f6a417454ffb8be01b95d76
Jason Pickens <jasonpickensnz@gmail.com>
2022-12-19 15:56:11 +1300

    Reproduce bug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,30 @@
-object Main extends App {
+import slinky.core.{FunctionalComponent, KeyAddingStage}
 
+import scala.scalajs.js
+
+object Main {
+
+  sealed trait TaggedProps[Props] extends Any
+
+  type RenderedProps[Props] = KeyAddingStage with TaggedProps[Props]
+
+  object Name {
+    case class Props(name: String)
+
+    val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props => props.name }
+
+    def apply(name: String): RenderedProps[Props] = component(Props(name)).asInstanceOf[RenderedProps[Props]]
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    val first: RenderedProps[Name.Props] = Name("Jason")
+    println(first)
+    val firstStage: KeyAddingStage = first
+    println(firstStage)
+    println(js.Object.is(first, firstStage))
+    val props: Seq[RenderedProps[Name.Props]] = Seq(first)
+    val propsHead: RenderedProps[Name.Props] = props.head
+    assert(js.Object.is(firstStage, propsHead))
+  }
 }
