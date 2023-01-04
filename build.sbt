e0084f5cf64c822007b395d4fd4c28aa1bbad893
Jason Pickens <jasonpickensnz@gmail.com>
2023-01-05 06:36:44 +1300

    Minimise

--- build.sbt
+++ build.sbt
@@ -1,9 +1,3 @@
 enablePlugins(ScalaJSPlugin, ScalaJSBundlerPlugin)
 scalaVersion := "2.13.10"
-libraryDependencies += "me.shadaj" %%% "slinky-core" % "0.7.3"
 scalaJSUseMainModuleInitializer := true
-Compile / npmDependencies ++= Seq(
-  "react" -> "18.2.0"
-)
-webpackCliVersion := "4.10.0"
-webpackDevServerPort := 8081
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,31 @@
-import slinky.core.{FunctionalComponent, KeyAddingStage}
-
 import scala.scalajs.js
 
 object Main {
 
   sealed trait TaggedProps[Props] extends Any
 
+  final class KeyAddingStage(private val args: js.Array[js.Any]) extends AnyVal
+
   type RenderedProps[Props] = KeyAddingStage with TaggedProps[Props]
 
   object Name {
     case class Props(name: String)
 
-    val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props => props.name }
+    def component(props: Props): KeyAddingStage = new KeyAddingStage(
+      js.Array(
+        props.asInstanceOf[js.Any]
+      )
+    )
 
     def apply(name: String): RenderedProps[Props] = component(Props(name)).asInstanceOf[RenderedProps[Props]]
   }
 
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
     val first: RenderedProps[Name.Props] = Name("Jason")
-    println(first)
     val firstStage: KeyAddingStage = first
-    println(firstStage)
     println(js.Object.is(first, firstStage))
     val props: Seq[RenderedProps[Name.Props]] = Seq(first)
     val propsHead: RenderedProps[Name.Props] = props.head
-    assert(js.Object.is(firstStage, propsHead))
+    println(js.Object.is(firstStage, propsHead))
   }
 }
