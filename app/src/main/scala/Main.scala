9d375d886b926c9ef00edb806f6eecb1ddc9ee3e
Jason Pickens <jasonpickensnz@gmail.com>
2023-04-21 06:38:31 +1200

    Reproduce multi-stage wrong level bug

---
+++ app/src/main/scala/Main.scala
@@ -0,0 +1,22 @@
+import reflect.Selectable.reflectiveSelectable
+import scala.annotation.experimental
+
+trait Bloop
+trait Blah
+
+@experimental
+@inspect
+object Outer {
+  class NoCtor()
+  class Foo[A <: String](val name: String)(using Int) extends Blah { myself: Bloop =>
+    println("constructed")
+    def yowza(i: Int): Unit = println("Yowza!" * i)
+  }
+}
+
+case class Person(name: String)
+
+object Main extends App {
+  val alice = data(Person("Alice"))
+//  println(alice.name)
+}
--- build.sbt
+++ build.sbt
@@ -1,1 +1,16 @@
-scalaVersion := "2.13.8"
+ThisBuild / scalaVersion := "3.3.0-RC4"
+
+lazy val root = project.in(file("."))
+  .aggregate(app, macros)
+  .settings(run := (app / Compile / run).evaluated)
+
+lazy val app = project
+  .dependsOn(macros)
+  .settings( //
+    scalacOptions += "-with-compiler"
+  )
+
+lazy val macros = project
+  .settings(//
+    scalacOptions ++= Seq("-with-compiler", "-feature")
+  )
