910e3e750f7d42ddd78a1be396361a8fe8bedc4b
Jason Pickens <jasonpickensnz@gmail.com>
2023-04-21 06:38:31 +1200

    Reproduce recursion bug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,30 @@
-object Main extends App {
+import scala.deriving.Mirror
+import reflect.Selectable.reflectiveSelectable
 
+trait WithSelf {
+  type Self
+}
+
+case class Demo(firstName: String, age: Int)
+
+object Demo {
+
+  type Data = WithSelf {
+    val firstName: String
+    def withFirstName(value: String): Self
+  }
+
+  private case class DemoData(firstName: String, age: Int) extends WithSelf {
+    type Self = DemoData
+    def withFirstName(value: String): Demo = Demo(value, age)
+  }
+
+  def data(demo: Demo): Data = DemoData(demo.firstName, demo.age).asInstanceOf[Data]
+}
+
+object Main extends App {
+  val demo = Demo("Bob", 69)
+  val demoData = Demo.data(demo)
+  println(demoData.firstName)
+  println(demoData.withFirstName("Jim"))
 }
