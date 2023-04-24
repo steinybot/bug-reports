c0f5be56615dc2e9edc8521a42637e7be123e49e
Jason Pickens <jasonpickensnz@gmail.com>
2023-04-24 16:05:24 +1200

    Minimise

--- src/main/scala/Bob.scala
+++
@@ -1,1 +0,0 @@
-trait Bob[B] {}
--- src/main/scala/Macro.scala
+++ src/main/scala/Macro.scala
@@ -1,4 +1,22 @@
 import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+trait Bob[B] {}
+
+object MacroImpl {
+
+  def focus_impl[A, B: c.WeakTypeTag](
+    c: blackbox.Context
+  )(path: c.Expr[A => B])(bob: c.Expr[Bob[B]]): c.Expr[Unit] = {
+    import c.universe._
+
+    val subj = c.prefix.tree
+
+    val _ = bob
+
+    c.Expr[Unit](q"""()""")
+  }
+}
 
 final case class Slot[A] protected (
   get: A,
