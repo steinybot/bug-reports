fefdaf50945a9f66bfad95c4264930cb829ab995
Jason Pickens <jasonpickensnz@gmail.com>
2024-03-21 12:10:32 +1300

    Minimise

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -2,22 +2,24 @@ package example
 
 import scala.scalajs.js
 
-trait TitleHandle extends js.Object {
-  def title(): String
-}
-
 object Main extends App {
 
-  val handle = {
-    // Inlining this fixes the problem.
-    // Moving it out of the current block scope also fixes the problem.
-    val routeTitle = "My page"
-    val handle = new TitleHandle {
-      override def title(): String = routeTitle
-    }
-    // This causes the private symbols to be lost. It is a js.Object though so JavaScript semantics should apply.
-    js.Object.assign(js.Object(), handle).asInstanceOf[TitleHandle]
+  trait Config extends js.Object {
+    def title(): String
   }
 
-  println(handle.title())
+  val config = {
+    // Inlining this fixes the problem.
+    // Moving it out of the current block scope also fixes the problem.
+    // It is getting stored as a private symbol on the config object.
+    val routeTitle = "My page"
+    val config = new Config {
+      override def title(): String = routeTitle
+    }
+    // This causes the private symbols to be lost.
+    // It is a js.Object though so JavaScript semantics should apply.
+    js.Object.assign(js.Object(), config).asInstanceOf[Config]
+  }
+
+  println(config.title())
 }
