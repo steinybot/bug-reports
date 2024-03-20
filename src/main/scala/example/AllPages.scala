3c3f98877a3224a3939d8bd5c7a14a825367678b
Jason Pickens <jasonpickensnz@gmail.com>
2024-03-21 11:42:49 +1300

    Minimise

--- src/main/scala/example/AllPages.scala
+++ src/main/scala/example/AllPages.scala
@@ -1,6 +1,5 @@
 package example
 
-import example.BaseRoute.staticTitle
 import example.Options.combineObjects
 
 import scala.scalajs.js
@@ -12,18 +11,12 @@ object AllPages {
 
 //  case object FnolPage extends AllPages with StaticRoute with RelayRoute {
   case object FnolPage extends AllPages {
-//    override val parent: UndefOr[BaseRoute]                           = DashboardLayout().Center
-    override val parent: UndefOr[BaseRoute]                           = js.undefined
     override val title: UndefOr[String] = "File a claim"
-    override val path: UndefOr[String]                                = "claim/new"
-//    override val entryPoint: RelayEntryPoint[_]                       = FileAClaimViewEntryPoint
 
     override def handle: Any = {
       val handle               = super.handle.asInstanceOf[js.Object]
-      // @Note Configured here: https://web.freshchat.com/a/121288663858798/settings/channels
       val freshchatHandle = FreshchatHandle(js.Array("claims-page"))
       combineObjects[js.Object, FreshchatHandle](handle, freshchatHandle)
     }
   }
-
 }
--- src/main/scala/example/BaseRoute.scala
+++ src/main/scala/example/BaseRoute.scala
@@ -1,37 +1,11 @@
 package example
 
-import slinky.core.facade.ReactElement
-
 import scala.scalajs.js
 import scala.scalajs.js.UndefOr
 
 trait BaseRoute {
 
-  val parent: UndefOr[BaseRoute]
-
   val title: UndefOr[String] = js.undefined
-
-  /** Path relative to the parent without a leading slash.
-   *
-   * May be `js.undefined` if this is the index page of a parent or some other intermediate layout component.
-   *
-   * Parameters will only match multiple segments if it has a pattern than matches a `/`.
-   */
-  val path: UndefOr[String] = js.undefined
-
-  /** The absolute pathname for the page starting with a leading slash.
-   */
-  def fullPath: String = {
-    val parentPath = parent.fold("")(_.fullPath)
-    path.fold(parentPath) { child =>
-      if (parentPath.isEmpty || parentPath.endsWith("/")) parentPath + child
-      else parentPath + "/" + child
-    }
-  }
-
-  val errorElement: ReactElement = null
-
-  //  val action: UndefOr[ActionFunctionArgs[Any] => js.Promise[DataFunctionValue]] = js.undefined
 
   def handle: Any = {
     val routeTitle = title
@@ -39,10 +13,4 @@ trait BaseRoute {
       override def title(m: UIMatch[Any, Any]): UndefOr[String] = routeTitle
     }
   }
-}
-
-object BaseRoute {
-
-  def staticTitle(title: String): UndefOr[UIMatch[Any, Any] => UndefOr[String]] =
-    (_: UIMatch[Any, Any]) => title
 }
