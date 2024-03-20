e8ca92946981252fd4d82a8901adb9658efc07a2
Jason Pickens <jasonpickensnz@gmail.com>
2024-03-21 11:54:15 +1300

    Minimise

--- src/main/scala/example/AllPages.scala
+++ src/main/scala/example/AllPages.scala
@@ -22,14 +22,13 @@ object AllPages {
 
     override def handle: Any = {
       val handle               = super.handle.asInstanceOf[js.Object]
-      val freshchatHandle = FreshchatHandle(js.Array("claims-page"))
 
       def combineObjects[A <: js.Object, B <: js.Object](first: A, second: B): A with B = {
         val result = js.Object.assign(js.Dynamic.literal(), first, second)
         result.asInstanceOf[A with B]
       }
 
-      combineObjects[js.Object, FreshchatHandle](handle, freshchatHandle)
+      combineObjects[js.Object, js.Object](handle, js.Object())
     }
   }
 }
